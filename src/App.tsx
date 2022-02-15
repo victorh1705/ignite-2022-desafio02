8ddf814eebafe35b7ed6159ee3179cf660191aa0
victor <victorh1705@gmail.com>
2022-02-15 00:29:47 -0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
-
 import {SideBar} from './components/SideBar';
-// import { Content } from './components/Content';
+import {Content} from './components/Content';
 
 import { api } from './services/api';
 
@@ -12,7 +9,6 @@ import './styles/global.scss';
 
 import './styles/sidebar.scss';
 import './styles/content.scss';
-import {Content} from './components/Content';
 
 interface GenreResponseProps {
   id: number;
